1055d6d567c61e85d617e1d756a5315bd78e92a4
Ahsan Khan <ahsankhanmail@gmail.com>
2015-11-19 18:20:45 -0800

    Format code

--- Sunshine/app/src/main/java/com/example/ahsankhan/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/ahsankhan/sunshine/MainActivity.java
@@ -1,20 +1,18 @@
 package com.example.ahsankhan.sunshine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import android.widget.ListView;
-import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-		.add(R.id.container, new PlaceholderFragment())
-		.commit();
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
         }
     }
 
@@ -54,24 +52,24 @@ public class MainActivity extends AppCompatActivity {
     public static class PlaceholderFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater,
-				 ViewGroup container,
-				 Bundle savedInstanceState) {
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.content_main,
-					     container, false);
-	    String [] forecasts = {
-		"Today Sunday 88/63",
-		"Tomorrow Monday 88/22",
-		"Tuesday 55/22",
-		"Wednesday 99/55"
-	    };
-	    ArrayList<String> items = new ArrayList<String>(Arrays.asList(forecasts));
-	    ArrayAdapter<String> adapter =
-		new ArrayAdapter<String>(getActivity(), // parent activity
-					 R.layout.list_item_laout,
-					 R.id.list_item_forecast_textview,
-					 items);
-	    ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-	    listView.setAdapter(adapter);
+                    container, false);
+            String[] forecasts = {
+                    "Today Sunday 88/63",
+                    "Tomorrow Monday 88/22",
+                    "Tuesday 55/22",
+                    "Wednesday 99/55"
+            };
+            ArrayList<String> items = new ArrayList<String>(Arrays.asList(forecasts));
+            ArrayAdapter<String> adapter =
+                    new ArrayAdapter<String>(getActivity(), // parent activity
+                            R.layout.list_item_laout,
+                            R.id.list_item_forecast_textview,
+                            items);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
             return rootView;
         }
     }
